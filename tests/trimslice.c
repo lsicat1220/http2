96a28332210b3b2149d11d694ee22fdba8313bcb
lorenzo <xyzhybrid@gmail.com>
2026-03-27 03:12:31 -0400

    feat: implemented test script for trim-slice

---
+++ tests/trimslice.c
@@ -0,0 +1,17 @@
+#include "../include/parsing.h"
+#include <stdio.h>
+
+int main() {
+	char tests[][64] = {"   this is a test   ", "the second test   ", "this shouldn't change"};
+	int num_tests = sizeof(tests) / 64;
+	Slice slice;
+	for (int i = 0; i < num_tests; i++) {
+		InitSlice(&slice, tests[i]);
+		printf("Initial string: ");
+		fwrite(slice.start, 1, slice.len, stdout);
+		printf("\n");
+		TrimSlice(&slice);
+		printf("Trimmed string: ");
+		fwrite(slice.start, 1, slice.len, stdout);
+	}
+}
