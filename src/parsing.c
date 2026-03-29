429a4fa235b8eb298cedd014d4596464cf9675d7
lorenzo <xyzhybrid@gmail.com>
2026-03-29 12:48:37 -0400

    feat: implemented GetVersion()

--- src/parsing.c
+++ src/parsing.c
@@ -13,3 +13,17 @@ int GetMethod(Slice* method) {
 	}
 	return -1;
 }
+// HTTP/1.1
+int GetVersion(Slice *version) {
+	if (version->len != 8 || memcmp(version->start, "HTTP/1.", 7)) {
+		return -1;
+	}
+	char ver = version->start[7];
+	switch (ver) {
+		case '0':
+			return 0;
+		case '1':
+			return 1;
+	}
+	return -1;
+}
